9e34c477b4a35df9a6df011299b5caeed00df77a
Vishnu Nair <vish@vnair.me>
2018-10-22 22:12:29 -0400

    Now using newspaper3k to parse each article from NewsAPI.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import settings
 from ebooklib import epub
 from jinja2 import Environment, FileSystemLoader
 from geopy.geocoders import Nominatim
+from newspaper import Article
 
 DARK_SKY_API_URL = "https://api.darksky.net/forecast/{}/{},{}"
 NEWS_API_URL = "https://newsapi.org/v2/top-headlines"
@@ -31,3 +32,27 @@ c = epub.EpubHtml(title="weather", file_name="weather.xhtml", lang='en')
 c.set_content(template.render(weather=r.json()))
 book.add_item(c)
 chaps.append(c)
+
+# news
+r = requests.get(NEWS_API_URL, params={'apiKey': settings.NEWS_API_KEY, 'country': "us"})
+# TODO: Error-check request.
+news_request_results = r.json()
+parsed_articles = []
+count = 0
+
+for a in news_request_results["articles"]:
+    pa = {
+        "count": count,
+        "source": a["source"]["name"],
+        "author": a["author"],
+        "title": a["title"],
+        "top_image": a["urlToImage"],
+        "desc": a["description"],
+        "url": a["url"]
+    }
+    this_article = Article(pa["url"])
+    this_article.download()
+    this_article.parse()
+    pa["article_text"] = this_article.text
+    count += 1
+    parsed_articles.append(pa)
